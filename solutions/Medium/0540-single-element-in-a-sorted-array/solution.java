c6a8e88e3abc730a1b6445530f3c376073bac3b1
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-18 05:56:47 +0000

    leetie: Add 540. Single Element in a Sorted Array [Medium] (java)

--- solutions/Medium/0540-single-element-in-a-sorted-array/solution.java
+++ solutions/Medium/0540-single-element-in-a-sorted-array/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Array, Binary Search
 // Link     : https://leetcode.com/problems/single-element-in-a-sorted-array/
 // Runtime  : 0 ms (beats 100%)
-// Memory   : 52640000 (beats 91%)
+// Memory   : 52456000 (beats 98%)
 // Language : java
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
