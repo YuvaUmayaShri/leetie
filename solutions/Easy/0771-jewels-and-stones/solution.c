6dfeb17459ee7e8de7b8f2d32568a6a50f86dc3b
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-18 17:57:49 +0000

    leetie: Add 771. Jewels and Stones [Easy] (c)

--- solutions/Easy/0771-jewels-and-stones/solution.c
+++ solutions/Easy/0771-jewels-and-stones/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String
 // Link     : https://leetcode.com/problems/jewels-and-stones/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8364000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8764000 (beats 39%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
