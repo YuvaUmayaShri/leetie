025c71063ee9efb701c676924eb75e5259d6ae8e
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-18 17:54:11 +0000

    leetie: Add 688. Knight Probability in Chessboard [Medium] (c)

--- solutions/Medium/0688-knight-probability-in-chessboard/solution.c
+++ solutions/Medium/0688-knight-probability-in-chessboard/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Dynamic Programming
 // Link     : https://leetcode.com/problems/knight-probability-in-chessboard/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 9232000 (beats 0%)
+// Runtime  : 9 ms (beats 13%)
+// Memory   : 8928000 (beats 53%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
