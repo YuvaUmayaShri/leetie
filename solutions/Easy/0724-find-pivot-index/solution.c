0fdb8170001c084b8b096650e85dee5232e71195
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-19 18:25:41 +0000

    leetie: Add 724. Find Pivot Index [Easy] (c)

--- solutions/Easy/0724-find-pivot-index/solution.c
+++ solutions/Easy/0724-find-pivot-index/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Prefix Sum
 // Link     : https://leetcode.com/problems/find-pivot-index/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8600000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10168000 (beats 95%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
