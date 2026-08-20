a385cb71416a32ffb2567887b326bd89fe5f1fa9
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-20 03:59:15 +0000

    leetie: Add 860. Lemonade Change [Easy] (c)

--- solutions/Easy/0860-lemonade-change/solution.c
+++ solutions/Easy/0860-lemonade-change/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Greedy
 // Link     : https://leetcode.com/problems/lemonade-change/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8600000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 15524000 (beats 1%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
