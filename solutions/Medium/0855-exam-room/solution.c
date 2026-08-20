a52de3d38ef7b64e29ad10cba59a3e674f2f9af9
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-20 03:55:54 +0000

    leetie: Add 855. Exam Room [Medium] (c)

--- solutions/Medium/0855-exam-room/solution.c
+++ solutions/Medium/0855-exam-room/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Design, Heap (Priority Queue), Ordered Set
 // Link     : https://leetcode.com/problems/exam-room/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8740000 (beats 0%)
+// Runtime  : 169 ms (beats 100%)
+// Memory   : 20092000 (beats 100%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
