a1ee2dcf1594776d4052a51e10c79e015b65755a
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-20 03:52:51 +0000

    leetie: Add 682. Baseball Game [Easy] (c)

--- solutions/Easy/0682-baseball-game/solution.c
+++ solutions/Easy/0682-baseball-game/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Stack, Simulation
 // Link     : https://leetcode.com/problems/baseball-game/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8464000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8972000 (beats 97%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
