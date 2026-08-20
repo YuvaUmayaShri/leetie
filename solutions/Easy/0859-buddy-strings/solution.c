615acd040e860d63ae1202c3ffe76caa88cbe6a7
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-20 03:56:49 +0000

    leetie: Add 859. Buddy Strings [Easy] (c)

--- solutions/Easy/0859-buddy-strings/solution.c
+++ solutions/Easy/0859-buddy-strings/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String
 // Link     : https://leetcode.com/problems/buddy-strings/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8372000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8872000 (beats 26%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
