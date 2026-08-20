98556ba3b453a1d147d58adf11cfd6a98e1a3c59
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-20 03:51:09 +0000

    leetie: Add 517. Super Washing Machines [Hard] (c)

--- solutions/Hard/0517-super-washing-machines/solution.c
+++ solutions/Hard/0517-super-washing-machines/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Greedy
 // Link     : https://leetcode.com/problems/super-washing-machines/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8436000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9184000 (beats 100%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
