df38944168d6689953353a2bc3e71edddc17e430
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-20 03:56:43 +0000

    leetie: Add 859. Buddy Strings [Easy] (c)

--- solutions/Easy/0859-buddy-strings/solution.c
+++ solutions/Easy/0859-buddy-strings/solution.c
@@ -3,81 +3,47 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String
 // Link     : https://leetcode.com/problems/buddy-strings/
-// Runtime  : N/A (beats 0%)
-// Memory   : N/A (beats 0%)
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8372000 (beats 0%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
 // ──────────────────────────────────────────────────
 
-#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
 
-typedef struct {
-    int N;
-    int* seats;
-    int count;
-} ExamRoom;
+bool buddyStrings(char* s, char* goal) {
+    int lenS = strlen(s);
+    int lenGoal = strlen(goal);
 
-ExamRoom* examRoomCreate(int n) {
-    ExamRoom* obj = (ExamRoom*)malloc(sizeof(ExamRoom));
-    obj->N = n;
-    obj->seats = (int*)malloc(10000 * sizeof(int));
-    obj->count = 0;
-    return obj;
-}
-
-int examRoomSeat(ExamRoom* obj) {
-    if (obj->count == 0) {
-        obj->seats[0] = 0;
-        obj->count++;
-        return 0;
+    if (lenS != lenGoal) {
+        return false;
     }
 
-    int maxDist = obj->seats[0];
-    int seatToTake = 0;
-    int insertIdx = 0;
+    if (strcmp(s, goal) == 0) {
+        int count[26] = {0};
+        for (int i = 0; i < lenS; i++) {
+            count[s[i] - 'a']++;
+            if (count[s[i] - 'a'] > 1) {
+                return true;
+            }
+        }
+        return false;
+    }
 
-    for (int i = 0; i < obj->count - 1; i++) {
-        int dist = (obj->seats[i + 1] - obj->seats[i]) / 2;
-        if (dist > maxDist) {
-            maxDist = dist;
-            seatToTake = obj->seats[i] + dist;
-            insertIdx = i + 1;
+    int first = -1, second = -1;
+    for (int i = 0; i < lenS; i++) {
+        if (s[i] != goal[i]) {
+            if (first == -1) {
+                first = i;
+            } else if (second == -1) {
+                second = i;
+            } else {
+                return false;
+            }
         }
     }
 
-    if ((obj->N - 1 - obj->seats[obj->count - 1]) > maxDist) {
-        seatToTake = obj->N - 1;
-        insertIdx = obj->count;
-    }
-
-    for (int i = obj->count; i > insertIdx; i--) {
-        obj->seats[i] = obj->seats[i - 1];
-    }
-    obj->seats[insertIdx] = seatToTake;
-    obj->count++;
-
-    return seatToTake;
-}
-
-void examRoomLeave(ExamRoom* obj, int p) {
-    int idx = -1;
-    for (int i = 0; i < obj->count; i++) {
-        if (obj->seats[i] == p) {
-            idx = i;
-            break;
-        }
-    }
-
-    if (idx != -1) {
-        for (int i = idx; i < obj->count - 1; i++) {
-            obj->seats[i] = obj->seats[i + 1];
-        }
-        obj->count--;
-    }
-}
-
-void examRoomFree(ExamRoom* obj) {
-    free(obj->seats);
-    free(obj);
+    return (second != -1 && s[first] == goal[second] && s[second] == goal[first]);
 }
