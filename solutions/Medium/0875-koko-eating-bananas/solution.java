c7e0fe9b1935b8ddb10792be717c55f184d5bf85
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-20 04:55:24 +0000

    leetie: Add 875. Koko Eating Bananas [Medium] (java)

--- solutions/Medium/0875-koko-eating-bananas/solution.java
+++ solutions/Medium/0875-koko-eating-bananas/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Array, Binary Search
 // Link     : https://leetcode.com/problems/koko-eating-bananas/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 42420000 (beats 0%)
+// Memory   : 41996000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
@@ -13,11 +13,10 @@
 class Solution {
     public int minEatingSpeed(int[] piles, int h) {
         int low=1;
-        int max=0;
         int mid;
         int high=0;
         for(int i=0;i<piles.length;i++){
-            high=Math.max(max,piles[i]);
+            high=Math.max(high,piles[i]);
         }
         while(low<=high){
             mid=(low+high)/2;
