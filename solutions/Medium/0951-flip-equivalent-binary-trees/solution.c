4d507c6367f98d17a1d0e906667f2d1ee66300d4
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 04:20:13 +0000

    leetie: Add 951. Flip Equivalent Binary Trees [Medium] (c)

--- solutions/Medium/0951-flip-equivalent-binary-trees/solution.c
+++ solutions/Medium/0951-flip-equivalent-binary-trees/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Tree, Depth-First Search, Binary Tree
 // Link     : https://leetcode.com/problems/flip-equivalent-binary-trees/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8776000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10044000 (beats 55%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
