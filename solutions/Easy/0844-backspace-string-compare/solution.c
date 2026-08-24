b98572687a67a6df6aada7aebe9ab9ed3cc7d518
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 03:48:10 +0000

    leetie: Add 844. Backspace String Compare [Easy] (c)

--- solutions/Easy/0844-backspace-string-compare/solution.c
+++ solutions/Easy/0844-backspace-string-compare/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Two Pointers, String, Stack, Simulation
 // Link     : https://leetcode.com/problems/backspace-string-compare/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8524000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8524000 (beats 82%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
