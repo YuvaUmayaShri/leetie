002996cc0788ff7f9e64dffe235bf4c87af0dfd6
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 04:17:15 +0000

    leetie: Add 630. Course Schedule III [Hard] (c)

--- solutions/Hard/0630-course-schedule-iii/solution.c
+++ solutions/Hard/0630-course-schedule-iii/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Greedy, Sorting, Heap (Priority Queue)
 // Link     : https://leetcode.com/problems/course-schedule-iii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8532000 (beats 0%)
+// Runtime  : 14 ms (beats 100%)
+// Memory   : 20952000 (beats 53%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
