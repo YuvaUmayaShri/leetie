d322af97b47ad227c0144f540986400d751cf032
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 03:28:49 +0000

    leetie: Add 129. Sum Root to Leaf Numbers [Medium] (c)

--- solutions/Medium/0129-sum-root-to-leaf-numbers/solution.c
+++ solutions/Medium/0129-sum-root-to-leaf-numbers/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Tree, Depth-First Search, Binary Tree
 // Link     : https://leetcode.com/problems/sum-root-to-leaf-numbers/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8276000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9340000 (beats 41%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
