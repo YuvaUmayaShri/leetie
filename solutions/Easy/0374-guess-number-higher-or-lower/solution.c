f637640582d6204d50401cf362f309e47db66313
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 03:31:55 +0000

    leetie: Add 374. Guess Number Higher or Lower [Easy] (c)

--- solutions/Easy/0374-guess-number-higher-or-lower/solution.c
+++ solutions/Easy/0374-guess-number-higher-or-lower/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Binary Search, Interactive
 // Link     : https://leetcode.com/problems/guess-number-higher-or-lower/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8500000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8512000 (beats 24%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
