c35a7d3120acecedf0f5878a7a56a6d6a1c202a9
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 04:01:10 +0000

    leetie: Add 798. Smallest Rotation with Highest Score [Hard] (c)

--- solutions/Hard/0798-smallest-rotation-with-highest-score/solution.c
+++ solutions/Hard/0798-smallest-rotation-with-highest-score/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Prefix Sum
 // Link     : https://leetcode.com/problems/smallest-rotation-with-highest-score/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8540000 (beats 0%)
+// Runtime  : 7 ms (beats 40%)
+// Memory   : 18736000 (beats 10%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
