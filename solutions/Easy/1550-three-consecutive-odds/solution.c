c8fb363eac518aace71f3ef68bd1cd93e63a6483
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 03:26:41 +0000

    leetie: Add 1550. Three Consecutive Odds [Easy] (c)

--- solutions/Easy/1550-three-consecutive-odds/solution.c
+++ solutions/Easy/1550-three-consecutive-odds/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array
 // Link     : https://leetcode.com/problems/three-consecutive-odds/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8584000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8744000 (beats 42%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
