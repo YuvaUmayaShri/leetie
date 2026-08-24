b2c20e10dd34fd4a323a2d2b1ddb9c3ae4356f3a
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-24 03:39:12 +0000

    leetie: Add 828. Count Unique Characters of All Substrings of a Given String [Hard] (c)

--- solutions/Hard/0828-count-unique-characters-of-all-substrings-of-a-given-string/solution.c
+++ solutions/Hard/0828-count-unique-characters-of-all-substrings-of-a-given-string/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Hash Table, String, Dynamic Programming
 // Link     : https://leetcode.com/problems/count-unique-characters-of-all-substrings-of-a-given-string/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8492000 (beats 0%)
+// Runtime  : 3 ms (beats 94%)
+// Memory   : 10268000 (beats 47%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
