3127456dfdc904f3b994e84b9a38368e8001bbbd
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-25 06:52:01 +0000

    leetie: Add 1283. Find the Smallest Divisor Given a Threshold [Medium] (java)

--- solutions/Medium/1283-find-the-smallest-divisor-given-a-threshold/solution.java
+++ solutions/Medium/1283-find-the-smallest-divisor-given-a-threshold/solution.java
@@ -3,15 +3,29 @@
 // Difficulty: Medium
 // Tags     : Array, Binary Search
 // Link     : https://leetcode.com/problems/find-the-smallest-divisor-given-a-threshold/
-// Runtime  : 6 ms (beats 99%)
-// Memory   : 50960000 (beats 74%)
+// Runtime  : 7 ms (beats 83%)
+// Memory   : 51156000 (beats 35%)
 // Language : java
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
 // ──────────────────────────────────────────────────
 
 class Solution {
+    public boolean possible(int[] nums,int threshold,int mid){
+        int sum=0;
+        for(int i=0;i<nums.length;i++){
+            sum+=(nums[i]+mid-1)/mid;
+        }
+        if(sum <= threshold){
+            return true;
+            
+        }else{
+            return false;
+        }
+
+    }
     public int smallestDivisor(int[] nums, int threshold) {
+        
         int[] arr = new int[10];
         int low=1;
         int high=0;
@@ -22,17 +36,12 @@ class Solution {
         int ans=high;
         while(low<=high){
             mid=(low+high)/2;
-            int sum=0;
-            for(int i=0;i<nums.length;i++){
-                sum+=(nums[i]+mid-1)/mid;
-            }
-            if(sum <= threshold){
-                ans= mid;
+            if(possible( nums,threshold, mid)){
+                ans=mid;
                 high=mid-1;
             }else{
-                low= mid+1;
+                low=mid+1;
             }
-
         }
         return ans;
         
