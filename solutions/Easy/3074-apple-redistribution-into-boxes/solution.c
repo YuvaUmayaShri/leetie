f358d3e31ef16cbc1c3f6530245e0cb690305383
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-25 04:40:27 +0000

    leetie: Add 3074. Apple Redistribution into Boxes [Easy] (c)

--- solutions/Easy/3074-apple-redistribution-into-boxes/solution.c
+++ solutions/Easy/3074-apple-redistribution-into-boxes/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Greedy, Sorting
 // Link     : https://leetcode.com/problems/apple-redistribution-into-boxes/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8652000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9780000 (beats 46%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
