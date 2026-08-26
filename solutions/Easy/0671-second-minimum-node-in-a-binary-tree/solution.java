84ea0139325fd297032c560cee8221cb3d442750
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-26 16:05:41 +0000

    leetie: Add 671. Second Minimum Node In a Binary Tree [Easy] (java)

--- solutions/Easy/0671-second-minimum-node-in-a-binary-tree/solution.java
+++ solutions/Easy/0671-second-minimum-node-in-a-binary-tree/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Tree, Depth-First Search, Binary Tree
 // Link     : https://leetcode.com/problems/second-minimum-node-in-a-binary-tree/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42628000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 42692000 (beats 64%)
 // Language : java
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
