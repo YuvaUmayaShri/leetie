816bfbd73253b5c7195bdf13b32d820ca18879af
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-26 15:58:28 +0000

    leetie: Add 538. Convert BST to Greater Tree [Medium] (java)

---
+++ solutions/Medium/0538-convert-bst-to-greater-tree/solution.java
@@ -0,0 +1,25 @@
+// ──────────────────────────────────────────────────
+// Problem  : 538. Convert BST to Greater Tree
+// Difficulty: Medium
+// Tags     : Tree, Depth-First Search, Binary Search Tree, Binary Tree
+// Link     : https://leetcode.com/problems/convert-bst-to-greater-tree/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 42692000 (beats 0%)
+// Language : java
+// Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    private int sum = 0;
+
+    public TreeNode convertBST(TreeNode root) {
+        if (root != null) {
+            convertBST(root.right);
+            sum += root.val;
+            root.val = sum;
+            convertBST(root.left);
+        }
+        return root;
+    }
+}
