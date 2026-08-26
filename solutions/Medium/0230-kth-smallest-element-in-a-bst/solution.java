7846fbb14fdca1ebe5c17b769853feb14bd715b1
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-26 15:52:34 +0000

    leetie: Add 230. Kth Smallest Element in a BST [Medium] (java)

--- solutions/Medium/0230-kth-smallest-element-in-a-bst/solution.java
+++ solutions/Medium/0230-kth-smallest-element-in-a-bst/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Tree, Depth-First Search, Binary Search Tree, Binary Tree
 // Link     : https://leetcode.com/problems/kth-smallest-element-in-a-bst/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42364000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 47092000 (beats 6%)
 // Language : java
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
