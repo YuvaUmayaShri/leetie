629db0a9443098a61815bbaea8be8248cb403e36
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-27 14:59:55 +0000

    leetie: Add 997. Find the Town Judge [Easy] (c)

--- solutions/Easy/0997-find-the-town-judge/solution.c
+++ solutions/Easy/0997-find-the-town-judge/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, Graph Theory
 // Link     : https://leetcode.com/problems/find-the-town-judge/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8628000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 20212000 (beats 93%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
