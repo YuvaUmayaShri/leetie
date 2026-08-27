c9840a78fe7b4fcd9c1257c8222a7ade6ff783fd
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-27 15:01:05 +0000

    leetie: Add 1094. Car Pooling [Medium] (c)

--- solutions/Medium/1094-car-pooling/solution.c
+++ solutions/Medium/1094-car-pooling/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Sorting, Heap (Priority Queue), Simulation, Prefix Sum
 // Link     : https://leetcode.com/problems/car-pooling/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8524000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10964000 (beats 41%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
