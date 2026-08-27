234e9dac2858a25f5c9fe97b234716f5b9c93ae6
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-27 14:53:27 +0000

    leetie: Add 965. Univalued Binary Tree [Easy] (c)

--- solutions/Easy/0965-univalued-binary-tree/solution.c
+++ solutions/Easy/0965-univalued-binary-tree/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Tree, Depth-First Search, Breadth-First Search, Binary Tree
 // Link     : https://leetcode.com/problems/univalued-binary-tree/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8468000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9404000 (beats 27%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
