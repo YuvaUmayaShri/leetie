b2bba0d44decf35c445d441934bc77573e2f8b43
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-28 03:56:49 +0000

    leetie: Add 2042. Check if Numbers Are Ascending in a Sentence [Easy] (c)

--- solutions/Easy/2042-check-if-numbers-are-ascending-in-a-sentence/solution.c
+++ solutions/Easy/2042-check-if-numbers-are-ascending-in-a-sentence/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : String
 // Link     : https://leetcode.com/problems/check-if-numbers-are-ascending-in-a-sentence/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8424000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8740000 (beats 41%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
