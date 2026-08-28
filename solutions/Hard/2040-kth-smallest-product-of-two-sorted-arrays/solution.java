ec242aa25bc7d2ca078505f6410d2c65af5c2070
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-28 03:56:03 +0000

    leetie: Add 2040. Kth Smallest Product of Two Sorted Arrays [Hard] (java)

--- solutions/Hard/2040-kth-smallest-product-of-two-sorted-arrays/solution.java
+++ solutions/Hard/2040-kth-smallest-product-of-two-sorted-arrays/solution.java
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Binary Search
 // Link     : https://leetcode.com/problems/kth-smallest-product-of-two-sorted-arrays/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 42652000 (beats 0%)
+// Runtime  : 768 ms (beats 22%)
+// Memory   : 62764000 (beats 61%)
 // Language : java
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
