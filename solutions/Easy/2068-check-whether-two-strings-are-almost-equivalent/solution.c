28d0cf632dcf4fd0a68b60c9bb86212235cf1942
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-28 04:48:42 +0000

    leetie: Add 2068. Check Whether Two Strings are Almost Equivalent [Easy] (c)

--- solutions/Easy/2068-check-whether-two-strings-are-almost-equivalent/solution.c
+++ solutions/Easy/2068-check-whether-two-strings-are-almost-equivalent/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String, Counting
 // Link     : https://leetcode.com/problems/check-whether-two-strings-are-almost-equivalent/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8564000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8712000 (beats 33%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
