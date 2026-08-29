4118857d299c522380e0bf5fd8adf4580b76021d
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-29 15:49:17 +0000

    leetie: Add 2515. Shortest Distance to Target String in a Circular Array [Easy] (c)

---
+++ solutions/Easy/2515-shortest-distance-to-target-string-in-a-circular-array/solution.c
@@ -0,0 +1,33 @@
+// ──────────────────────────────────────────────────
+// Problem  : 2515. Shortest Distance to Target String in a Circular Array
+// Difficulty: Easy
+// Tags     : Array, String
+// Link     : https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8616000 (beats 0%)
+// Language : c
+// Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+#include <string.h>
+#include <stdlib.h>
+#include <math.h>
+
+int closestTarget(char** words, int wordsSize, char* target, int startIndex) {
+    int min_dist = wordsSize;
+
+    for (int i = 0; i < wordsSize; i++) {
+        if (strcmp(words[i], target) == 0) {
+            int abs_dist = abs(i - startIndex);
+            int circular_dist = wordsSize - abs_dist;
+            
+            int dist = abs_dist < circular_dist ? abs_dist : circular_dist;
+            if (dist < min_dist) {
+                min_dist = dist;
+            }
+        }
+    }
+
+    return min_dist == wordsSize ? -1 : min_dist;
+}
