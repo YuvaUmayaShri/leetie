9ef7319cd456bc674e2942eb46fc8a368074c9ab
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-29 15:49:24 +0000

    leetie: Add 2515. Shortest Distance to Target String in a Circular Array [Easy] (c)

--- solutions/Easy/2515-shortest-distance-to-target-string-in-a-circular-array/solution.c
+++ solutions/Easy/2515-shortest-distance-to-target-string-in-a-circular-array/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, String
 // Link     : https://leetcode.com/problems/shortest-distance-to-target-string-in-a-circular-array/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8616000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 11872000 (beats 77%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
