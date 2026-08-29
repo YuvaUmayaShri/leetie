c081dc1e8a0668596c3bf2de5688d682a34dab49
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-29 09:41:27 +0000

    leetie: Add 990. Satisfiability of Equality Equations [Medium] (c)

--- solutions/Medium/0990-satisfiability-of-equality-equations/solution.c
+++ solutions/Medium/0990-satisfiability-of-equality-equations/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, String, Union-Find, Graph Theory
 // Link     : https://leetcode.com/problems/satisfiability-of-equality-equations/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8256000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9224000 (beats 60%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
