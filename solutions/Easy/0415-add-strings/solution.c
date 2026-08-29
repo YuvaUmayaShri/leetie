bdfa91391a80d624dbb55b25bee510206c8e5af5
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-29 09:49:58 +0000

    leetie: Add 415. Add Strings [Easy] (c)

--- solutions/Easy/0415-add-strings/solution.c
+++ solutions/Easy/0415-add-strings/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, String, Simulation
 // Link     : https://leetcode.com/problems/add-strings/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8652000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9000000 (beats 43%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
