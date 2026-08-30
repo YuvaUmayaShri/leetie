5b9891a691ac61da1e6d3bb1dedf40e85f7d7c7c
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-30 08:39:48 +0000

    leetie: Add 1913. Maximum Product Difference Between Two Pairs [Easy] (c)

--- solutions/Easy/1913-maximum-product-difference-between-two-pairs/solution.c
+++ solutions/Easy/1913-maximum-product-difference-between-two-pairs/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Sorting, Quicksort
 // Link     : https://leetcode.com/problems/maximum-product-difference-between-two-pairs/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8484000 (beats 0%)
+// Runtime  : 20 ms (beats 56%)
+// Memory   : 10144000 (beats 47%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
