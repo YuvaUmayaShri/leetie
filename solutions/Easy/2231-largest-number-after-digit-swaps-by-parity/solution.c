199cddf4f46ccfc31b2d54947aaaace576a7dbba
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-30 08:43:05 +0000

    leetie: Add 2231. Largest Number After Digit Swaps by Parity [Easy] (c)

--- solutions/Easy/2231-largest-number-after-digit-swaps-by-parity/solution.c
+++ solutions/Easy/2231-largest-number-after-digit-swaps-by-parity/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Sorting, Heap (Priority Queue)
 // Link     : https://leetcode.com/problems/largest-number-after-digit-swaps-by-parity/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8416000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8772000 (beats 58%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
