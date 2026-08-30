77e84a4e1fd6dfcb60eaec83f8ef88b1f327e59d
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-30 08:32:23 +0000

    leetie: Add 47. Permutations II [Medium] (c)

--- solutions/Medium/0047-permutations-ii/solution.c
+++ solutions/Medium/0047-permutations-ii/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Backtracking, Sorting
 // Link     : https://leetcode.com/problems/permutations-ii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8604000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 14444000 (beats 69%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
