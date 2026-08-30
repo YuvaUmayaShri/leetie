0c2e52efc7ba4407f39a370e1296819098d31eb6
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-30 08:41:26 +0000

    leetie: Add 2089. Find Target Indices After Sorting Array [Easy] (c)

--- solutions/Easy/2089-find-target-indices-after-sorting-array/solution.c
+++ solutions/Easy/2089-find-target-indices-after-sorting-array/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Binary Search, Sorting
 // Link     : https://leetcode.com/problems/find-target-indices-after-sorting-array/
-// Runtime  : 4 ms (beats 0%)
-// Memory   : 8596000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9460000 (beats 35%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
