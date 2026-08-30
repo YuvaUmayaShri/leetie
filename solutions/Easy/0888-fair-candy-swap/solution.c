27c14aad5f1a1b4c88fa632fe7eb22255f05b263
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-30 08:35:21 +0000

    leetie: Add 888. Fair Candy Swap [Easy] (c)

--- solutions/Easy/0888-fair-candy-swap/solution.c
+++ solutions/Easy/0888-fair-candy-swap/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, Binary Search, Sorting
 // Link     : https://leetcode.com/problems/fair-candy-swap/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8772000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 11176000 (beats 31%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
