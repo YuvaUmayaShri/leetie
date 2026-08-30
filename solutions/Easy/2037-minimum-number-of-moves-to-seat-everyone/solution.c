02284d46bce25215c4d9877c65755a311a2a1d17
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-30 08:40:49 +0000

    leetie: Add 2037. Minimum Number of Moves to Seat Everyone [Easy] (c)

--- solutions/Easy/2037-minimum-number-of-moves-to-seat-everyone/solution.c
+++ solutions/Easy/2037-minimum-number-of-moves-to-seat-everyone/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Greedy, Sorting, Counting Sort
 // Link     : https://leetcode.com/problems/minimum-number-of-moves-to-seat-everyone/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8480000 (beats 0%)
+// Runtime  : 3 ms (beats 29%)
+// Memory   : 9456000 (beats 22%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
