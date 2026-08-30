f878bae857bd88680089423899e8f52cf38e045a
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-30 08:39:16 +0000

    leetie: Add 1029. Two City Scheduling [Medium] (c)

--- solutions/Medium/1029-two-city-scheduling/solution.c
+++ solutions/Medium/1029-two-city-scheduling/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Greedy, Sorting, Hungarian Algorithm, Successive Shortest Path Algorithm
 // Link     : https://leetcode.com/problems/two-city-scheduling/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8612000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10028000 (beats 100%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
