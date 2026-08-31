07076b62ade7e9b9d2e9086177205092d7ef2851
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-31 03:11:16 +0000

    leetie: Add 744. Find Smallest Letter Greater Than Target [Easy] (c)

--- solutions/Easy/0744-find-smallest-letter-greater-than-target/solution.c
+++ solutions/Easy/0744-find-smallest-letter-greater-than-target/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Binary Search
 // Link     : https://leetcode.com/problems/find-smallest-letter-greater-than-target/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8416000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9036000 (beats 83%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
