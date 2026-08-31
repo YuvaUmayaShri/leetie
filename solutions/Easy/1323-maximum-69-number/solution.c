e63e5e73ef23a206d3a5c0cf30f190e39d82b0d5
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-31 03:39:54 +0000

    leetie: Add 1323. Maximum 69 Number [Easy] (c)

--- solutions/Easy/1323-maximum-69-number/solution.c
+++ solutions/Easy/1323-maximum-69-number/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Math, Greedy
 // Link     : https://leetcode.com/problems/maximum-69-number/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8748000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8608000 (beats 41%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
