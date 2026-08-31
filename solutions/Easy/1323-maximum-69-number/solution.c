90a4c667d9258a675d491890a50e71161994109a
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-31 03:39:49 +0000

    leetie: Add 1323. Maximum 69 Number [Easy] (c)

---
+++ solutions/Easy/1323-maximum-69-number/solution.c
@@ -0,0 +1,30 @@
+// ──────────────────────────────────────────────────
+// Problem  : 1323. Maximum 69 Number
+// Difficulty: Easy
+// Tags     : Math, Greedy
+// Link     : https://leetcode.com/problems/maximum-69-number/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8748000 (beats 0%)
+// Language : c
+// Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+#include <stdio.h>
+#include <string.h>
+
+int maximum69Number(int num) {
+    char str[6];
+    sprintf(str, "%d", num);
+
+    for (int i = 0; str[i] != '\0'; i++) {
+        if (str[i] == '6') {
+            str[i] = '9';
+            break;
+        }
+    }
+
+    int result;
+    sscanf(str, "%d", &result);
+    return result;
+}
