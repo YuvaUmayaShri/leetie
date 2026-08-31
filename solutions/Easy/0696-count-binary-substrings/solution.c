f424e7d544aba5de6c7fc6b037d73b3a90acb587
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-31 03:09:15 +0000

    leetie: Add 696. Count Binary Substrings [Easy] (c)

--- solutions/Easy/0696-count-binary-substrings/solution.c
+++ solutions/Easy/0696-count-binary-substrings/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Two Pointers, String
 // Link     : https://leetcode.com/problems/count-binary-substrings/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8488000 (beats 0%)
+// Runtime  : 7 ms (beats 19%)
+// Memory   : 9976000 (beats 9%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
