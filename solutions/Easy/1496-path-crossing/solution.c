35f843f918611881c81f47bcc2cbaa401440a9b1
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-31 03:40:32 +0000

    leetie: Add 1496. Path Crossing [Easy] (c)

--- solutions/Easy/1496-path-crossing/solution.c
+++ solutions/Easy/1496-path-crossing/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String
 // Link     : https://leetcode.com/problems/path-crossing/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8768000 (beats 0%)
+// Runtime  : 3 ms (beats 25%)
+// Memory   : 9100000 (beats 78%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
