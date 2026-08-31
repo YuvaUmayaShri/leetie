3ff1a710d5add4713bdc0d0e9bac5cca83601c21
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-08-31 03:39:20 +0000

    leetie: Add 1299. Replace Elements with Greatest Element on Right Side [Easy] (c)

--- solutions/Easy/1299-replace-elements-with-greatest-element-on-right-side/solution.c
+++ solutions/Easy/1299-replace-elements-with-greatest-element-on-right-side/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array
 // Link     : https://leetcode.com/problems/replace-elements-with-greatest-element-on-right-side/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8608000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 64320000 (beats 12%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
