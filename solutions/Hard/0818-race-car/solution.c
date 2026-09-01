522a1f695676e54ed5c9f42b1759c05cfa516eb7
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-01 06:06:58 +0000

    leetie: Add 818. Race Car [Hard] (c)

--- solutions/Hard/0818-race-car/solution.c
+++ solutions/Hard/0818-race-car/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Dynamic Programming, Heuristic Search, A* Search
 // Link     : https://leetcode.com/problems/race-car/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8588000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8744000 (beats 80%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
