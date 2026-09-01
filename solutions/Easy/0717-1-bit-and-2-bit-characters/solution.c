b9a149cbfa2d0ee92fa514ebfe2d67cedbaddf2c
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-01 05:56:05 +0000

    leetie: Add 717. 1-bit and 2-bit Characters [Easy] (c)

--- solutions/Easy/0717-1-bit-and-2-bit-characters/solution.c
+++ solutions/Easy/0717-1-bit-and-2-bit-characters/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array
 // Link     : https://leetcode.com/problems/1-bit-and-2-bit-characters/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8624000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8916000 (beats 37%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
