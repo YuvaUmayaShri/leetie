a7d624714fd194c97728565c4dafa89afb2f134a
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-02 06:51:34 +0000

    leetie: Add 118. Pascal's Triangle [Easy] (c)

--- solutions/Easy/0118-pascals-triangle/solution.c
+++ solutions/Easy/0118-pascals-triangle/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Dynamic Programming
 // Link     : https://leetcode.com/problems/pascals-triangle/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8636000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 11508000 (beats 70%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
