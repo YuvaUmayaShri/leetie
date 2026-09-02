39bf41e3d9294e268fe96bb322c2725eb1d50700
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-02 06:49:12 +0000

    leetie: Add 24. Swap Nodes in Pairs [Medium] (c)

--- solutions/Medium/0024-swap-nodes-in-pairs/solution.c
+++ solutions/Medium/0024-swap-nodes-in-pairs/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Linked List, Recursion
 // Link     : https://leetcode.com/problems/swap-nodes-in-pairs/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8636000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9552000 (beats 52%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
