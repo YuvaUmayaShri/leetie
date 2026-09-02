cf33b340d6d12e32f3f6d698232d4f0ef7f95594
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-02 06:54:46 +0000

    leetie: Add 141. Linked List Cycle [Easy] (c)

---
+++ solutions/Easy/0141-linked-list-cycle/solution.c
@@ -0,0 +1,31 @@
+// ──────────────────────────────────────────────────
+// Problem  : 141. Linked List Cycle
+// Difficulty: Easy
+// Tags     : Hash Table, Linked List, Two Pointers, Floyd's Cycle Finding Algorithm
+// Link     : https://leetcode.com/problems/linked-list-cycle/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8676000 (beats 0%)
+// Language : c
+// Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+bool hasCycle(struct ListNode *head) {
+    if (head == NULL || head->next == NULL) {
+        return false;
+    }
+
+    struct ListNode *slow = head;
+    struct ListNode *fast = head;
+
+    while (fast != NULL && fast->next != NULL) {
+        slow = slow->next;
+        fast = fast->next->next;
+
+        if (slow == fast) {
+            return true;
+        }
+    }
+
+    return false;
+}
