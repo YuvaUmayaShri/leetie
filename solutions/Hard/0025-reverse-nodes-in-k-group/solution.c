62cb815734d14efd7f5bad6ffcbbdb6cac425373
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-02 06:49:54 +0000

    leetie: Add 25. Reverse Nodes in k-Group [Hard] (c)

--- solutions/Hard/0025-reverse-nodes-in-k-group/solution.c
+++ solutions/Hard/0025-reverse-nodes-in-k-group/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Linked List, Recursion
 // Link     : https://leetcode.com/problems/reverse-nodes-in-k-group/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8752000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 12292000 (beats 46%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
