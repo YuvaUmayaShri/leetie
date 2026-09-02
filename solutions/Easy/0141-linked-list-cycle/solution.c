1f7fc9b056ef4a7e73df4dddd72d135ce7f39986
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-02 06:54:52 +0000

    leetie: Add 141. Linked List Cycle [Easy] (c)

--- solutions/Easy/0141-linked-list-cycle/solution.c
+++ solutions/Easy/0141-linked-list-cycle/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, Linked List, Two Pointers, Floyd's Cycle Finding Algorithm
 // Link     : https://leetcode.com/problems/linked-list-cycle/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8676000 (beats 0%)
+// Runtime  : 7 ms (beats 92%)
+// Memory   : 12012000 (beats 7%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
