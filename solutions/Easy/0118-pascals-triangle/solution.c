bfd55b2c88f771e8b447bdaa3efe49e35b2091c7
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-02 06:51:29 +0000

    leetie: Add 118. Pascal's Triangle [Easy] (c)

---
+++ solutions/Easy/0118-pascals-triangle/solution.c
@@ -0,0 +1,33 @@
+// ──────────────────────────────────────────────────
+// Problem  : 118. Pascal's Triangle
+// Difficulty: Easy
+// Tags     : Array, Dynamic Programming
+// Link     : https://leetcode.com/problems/pascals-triangle/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8636000 (beats 0%)
+// Language : c
+// Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+#include <stdlib.h>
+
+int** generate(int numRows, int* returnSize, int** returnColumnSizes) {
+    *returnSize = numRows;
+    int** triangle = (int**)malloc(numRows * sizeof(int*));
+    *returnColumnSizes = (int*)malloc(numRows * sizeof(int));
+
+    for (int i = 0; i < numRows; i++) {
+        (*returnColumnSizes)[i] = i + 1;
+        triangle[i] = (int*)malloc((i + 1) * sizeof(int));
+
+        triangle[i][0] = 1;
+        triangle[i][i] = 1;
+
+        for (int j = 1; j < i; j++) {
+            triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
+        }
+    }
+
+    return triangle;
+}
