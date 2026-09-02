4a7ed418efd79fa796f588c1c62f3fa2f77d70ee
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-02 06:50:20 +0000

    leetie: Add 74. Search a 2D Matrix [Medium] (c)

--- solutions/Medium/0074-search-a-2d-matrix/solution.c
+++ solutions/Medium/0074-search-a-2d-matrix/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Binary Search, Matrix
 // Link     : https://leetcode.com/problems/search-a-2d-matrix/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8636000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9508000 (beats 56%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
