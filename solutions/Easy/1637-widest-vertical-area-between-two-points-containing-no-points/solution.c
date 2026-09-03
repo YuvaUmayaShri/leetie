3a86a0f071c265b27182473045d3cd436f6577a1
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-03 04:35:11 +0000

    leetie: Add 1637. Widest Vertical Area Between Two Points Containing No Points [Easy] (c)

--- solutions/Easy/1637-widest-vertical-area-between-two-points-containing-no-points/solution.c
+++ solutions/Easy/1637-widest-vertical-area-between-two-points-containing-no-points/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Sorting
 // Link     : https://leetcode.com/problems/widest-vertical-area-between-two-points-containing-no-points/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8492000 (beats 0%)
+// Runtime  : 27 ms (beats 21%)
+// Memory   : 32688000 (beats 11%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
