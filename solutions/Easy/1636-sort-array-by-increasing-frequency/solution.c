c53d60b1b819e5281a6fcea960f24a1069371817
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-03 04:34:22 +0000

    leetie: Add 1636. Sort Array by Increasing Frequency [Easy] (c)

--- solutions/Easy/1636-sort-array-by-increasing-frequency/solution.c
+++ solutions/Easy/1636-sort-array-by-increasing-frequency/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Hash Table, Sorting
 // Link     : https://leetcode.com/problems/sort-array-by-increasing-frequency/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8596000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 11936000 (beats 63%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
