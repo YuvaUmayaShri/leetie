d2e29140543d16a7a0553b16ed90822eadbf94ab
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-04 16:17:04 +0000

    leetie: Add 1238. Circular Permutation in Binary Representation [Medium] (c)

--- solutions/Medium/1238-circular-permutation-in-binary-representation/solution.c
+++ solutions/Medium/1238-circular-permutation-in-binary-representation/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Backtracking, Bit Manipulation
 // Link     : https://leetcode.com/problems/circular-permutation-in-binary-representation/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8388000 (beats 0%)
+// Runtime  : 198 ms (beats 100%)
+// Memory   : 40140000 (beats 38%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
