f4dca928e239d19a702c24c735d566a079009e99
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-04 16:17:38 +0000

    leetie: Add 1239. Maximum Length of a Concatenated String with Unique Characters [Medium] (c)

--- solutions/Medium/1239-maximum-length-of-a-concatenated-string-with-unique-characters/solution.c
+++ solutions/Medium/1239-maximum-length-of-a-concatenated-string-with-unique-characters/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, String, Backtracking, Bit Manipulation
 // Link     : https://leetcode.com/problems/maximum-length-of-a-concatenated-string-with-unique-characters/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8456000 (beats 0%)
+// Runtime  : 3 ms (beats 25%)
+// Memory   : 8764000 (beats 25%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
