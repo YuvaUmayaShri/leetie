0ecce1e24eee93dfa1bdeb84e8d5c228ed8847b3
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-04 16:19:10 +0000

    leetie: Add 2056. Number of Valid Move Combinations On Chessboard [Hard] (c)

--- solutions/Hard/2056-number-of-valid-move-combinations-on-chessboard/solution.c
+++ solutions/Hard/2056-number-of-valid-move-combinations-on-chessboard/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, String, Backtracking, Simulation
 // Link     : https://leetcode.com/problems/number-of-valid-move-combinations-on-chessboard/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8584000 (beats 0%)
+// Runtime  : 63 ms (beats 100%)
+// Memory   : 9280000 (beats 0%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
