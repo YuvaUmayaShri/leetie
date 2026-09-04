512294ac40752a161b739765fae4911255d5191c
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-04 16:18:36 +0000

    leetie: Add 1863. Sum of All Subset XOR Totals [Easy] (c)

--- solutions/Easy/1863-sum-of-all-subset-xor-totals/solution.c
+++ solutions/Easy/1863-sum-of-all-subset-xor-totals/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Array, Math, Backtracking, Bit Manipulation, Combinatorics, Enumeration
 // Link     : https://leetcode.com/problems/sum-of-all-subset-xor-totals/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8500000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8684000 (beats 31%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
