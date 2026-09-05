0203fad7e09b5ff9e6b080a6b661b3af18fd6861
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-05 16:12:36 +0000

    leetie: Add 2181. Merge Nodes in Between Zeros [Medium] (c)

--- solutions/Medium/2181-merge-nodes-in-between-zeros/solution.c
+++ solutions/Medium/2181-merge-nodes-in-between-zeros/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Linked List, Simulation
 // Link     : https://leetcode.com/problems/merge-nodes-in-between-zeros/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8352000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 89328000 (beats 63%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
