3f2cbf6e6aa22b4d764e8aa810c64369518aeac5
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-05 16:13:05 +0000

    leetie: Add 2296. Design a Text Editor [Hard] (c)

--- solutions/Hard/2296-design-a-text-editor/solution.c
+++ solutions/Hard/2296-design-a-text-editor/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Linked List, String, Stack, Design, Simulation, Doubly-Linked List
 // Link     : https://leetcode.com/problems/design-a-text-editor/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8984000 (beats 0%)
+// Runtime  : 47 ms (beats 29%)
+// Memory   : 75108000 (beats 71%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
