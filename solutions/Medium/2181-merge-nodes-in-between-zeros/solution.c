a13a645682647f0dc496bbe09d4984f8e803a84f
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-05 16:12:27 +0000

    leetie: Add 2181. Merge Nodes in Between Zeros [Medium] (c)

---
+++ solutions/Medium/2181-merge-nodes-in-between-zeros/solution.c
@@ -0,0 +1,39 @@
+// ──────────────────────────────────────────────────
+// Problem  : 2181. Merge Nodes in Between Zeros
+// Difficulty: Medium
+// Tags     : Linked List, Simulation
+// Link     : https://leetcode.com/problems/merge-nodes-in-between-zeros/
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8352000 (beats 0%)
+// Language : c
+// Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+
+struct ListNode* mergeNodes(struct ListNode* head) {
+    struct ListNode* modify = head->next;
+    struct ListNode* traverse = head->next;
+
+    while (traverse != NULL) {
+        int sum = 0;
+        while (traverse->val != 0) {
+            sum += traverse->val;
+            traverse = traverse->next;
+        }
+
+        modify->val = sum;
+        traverse = traverse->next;
+        modify->next = (traverse != NULL) ? modify->next : NULL;
+        modify = modify->next;
+    }
+
+    return head->next;
+}
