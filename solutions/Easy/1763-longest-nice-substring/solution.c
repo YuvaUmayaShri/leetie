bad7a393bf38a383570f7b8a72060e5878e42856
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-06 17:10:00 +0000

    leetie: Add 1763. Longest Nice Substring [Easy] (c)

--- solutions/Easy/1763-longest-nice-substring/solution.c
+++ solutions/Easy/1763-longest-nice-substring/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Easy
 // Tags     : Hash Table, String, Divide and Conquer, Bit Manipulation, Sliding Window
 // Link     : https://leetcode.com/problems/longest-nice-substring/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8436000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10048000 (beats 20%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
