f3490db90000427ece2dadc452d64f3b2406dbe8
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-07 06:19:48 +0000

    leetie: Add 376. Wiggle Subsequence [Medium] (c)

--- solutions/Medium/0376-wiggle-subsequence/solution.c
+++ solutions/Medium/0376-wiggle-subsequence/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming, Greedy
 // Link     : https://leetcode.com/problems/wiggle-subsequence/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8476000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8912000 (beats 11%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
