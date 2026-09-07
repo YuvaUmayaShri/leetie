46e48c435023cdebe24dc1d2231be5a7b7db7554
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-07 06:19:12 +0000

    leetie: Add 373. Find K Pairs with Smallest Sums [Medium] (c)

--- solutions/Medium/0373-find-k-pairs-with-smallest-sums/solution.c
+++ solutions/Medium/0373-find-k-pairs-with-smallest-sums/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Heap (Priority Queue)
 // Link     : https://leetcode.com/problems/find-k-pairs-with-smallest-sums/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8748000 (beats 0%)
+// Runtime  : 30 ms (beats 32%)
+// Memory   : 61056000 (beats 90%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
