4dc83086af647e672e2be8c937e25d0eb5ca3476
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-07 17:55:52 +0000

    leetie: Add 74. Search a 2D Matrix [Medium] (java)

---
+++ solutions/Medium/0074-search-a-2d-matrix/solution.java
@@ -0,0 +1,34 @@
+// ──────────────────────────────────────────────────
+// Problem  : 74. Search a 2D Matrix
+// Difficulty: Medium
+// Tags     : Array, Binary Search, Matrix
+// Link     : https://leetcode.com/problems/search-a-2d-matrix/
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 43800000 (beats 80%)
+// Language : java
+// Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
+// Synced by: leetie
+// ──────────────────────────────────────────────────
+
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int n =matrix.length;
+        int m=matrix[0].length;
+        int low=0;
+        int high =(n*m-1);
+        int mid=0;
+        while(low<=high){
+            mid=(low+high)/2;
+            if(matrix[mid/m][mid%m]==target){
+                return true;
+            }
+            if(matrix[mid/m][mid%m]<target){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
+        }
+        return false;
+        
+    }
+}
