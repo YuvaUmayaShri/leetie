5877654c70de1438493512e46cd8ffa83c68faaf
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-07 06:18:27 +0000

    leetie: Add 229. Majority Element II [Medium] (c)

--- solutions/Medium/0229-majority-element-ii/solution.c
+++ solutions/Medium/0229-majority-element-ii/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Hash Table, Sorting, Counting, Boyer–Moore Majority Vote Algorithm
 // Link     : https://leetcode.com/problems/majority-element-ii/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8628000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 11204000 (beats 26%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
