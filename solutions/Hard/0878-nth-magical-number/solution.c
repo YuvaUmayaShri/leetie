0b761e7755b55ffef586691c24894517fbddc0fc
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-08 09:12:45 +0000

    leetie: Add 878. Nth Magical Number [Hard] (c)

--- solutions/Hard/0878-nth-magical-number/solution.c
+++ solutions/Hard/0878-nth-magical-number/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Math, Binary Search, Least Common Multiple, Inclusion-Exclusion Principle
 // Link     : https://leetcode.com/problems/nth-magical-number/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8616000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8580000 (beats 37%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
