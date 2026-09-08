7398a9c48041453ad8b17ea3cee2bfe1023490e2
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-08 09:16:46 +0000

    leetie: Add 887. Super Egg Drop [Hard] (c)

--- solutions/Hard/0887-super-egg-drop/solution.c
+++ solutions/Hard/0887-super-egg-drop/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Math, Binary Search, Dynamic Programming
 // Link     : https://leetcode.com/problems/super-egg-drop/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8580000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8628000 (beats 47%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
