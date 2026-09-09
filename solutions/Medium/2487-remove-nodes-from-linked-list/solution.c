8c2909a5c7b292410d81b85d157c522ae6a2c54e
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-09 18:08:56 +0000

    leetie: Add 2487. Remove Nodes From Linked List [Medium] (c)

--- solutions/Medium/2487-remove-nodes-from-linked-list/solution.c
+++ solutions/Medium/2487-remove-nodes-from-linked-list/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Linked List, Stack, Recursion, Monotonic Stack
 // Link     : https://leetcode.com/problems/remove-nodes-from-linked-list/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8668000 (beats 0%)
+// Runtime  : 6 ms (beats 70%)
+// Memory   : 62800000 (beats 63%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
