11b5dcbad8d231a9cfe2d858e0da4f35586754a2
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-09 18:06:07 +0000

    leetie: Add 1721. Swapping Nodes in a Linked List [Medium] (c)

--- solutions/Medium/1721-swapping-nodes-in-a-linked-list/solution.c
+++ solutions/Medium/1721-swapping-nodes-in-a-linked-list/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Linked List, Two Pointers
 // Link     : https://leetcode.com/problems/swapping-nodes-in-a-linked-list/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8528000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 53392000 (beats 98%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
