83c3f25e58887517b112d58417dadadf7fe47ddb
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-12 16:44:25 +0000

    leetie: Add 131. Palindrome Partitioning [Medium] (c)

--- solutions/Medium/0131-palindrome-partitioning/solution.c
+++ solutions/Medium/0131-palindrome-partitioning/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : String, Dynamic Programming, Backtracking
 // Link     : https://leetcode.com/problems/palindrome-partitioning/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8608000 (beats 0%)
+// Runtime  : 104 ms (beats 63%)
+// Memory   : 94220000 (beats 76%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
