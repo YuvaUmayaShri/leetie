1462fc767ace8ffd6e39471443a6a6af6dd03bef
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-15 13:01:37 +0000

    leetie: Add 1. Two Sum [Easy] (java)

--- solutions/Easy/0001-two-sum/solution.java
+++ solutions/Easy/0001-two-sum/solution.java
@@ -4,7 +4,7 @@
 // Tags     : Array, Hash Table
 // Link     : https://leetcode.com/problems/two-sum/
 // Runtime  : 0 ms (beats 0%)
-// Memory   : 42744000 (beats 0%)
+// Memory   : 42388000 (beats 0%)
 // Language : java
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
@@ -12,20 +12,13 @@
 
 class Solution {
     public int[] twoSum(int[] nums, int target) {
-        int[] arr= new int[2];
-        int l=0;
-        int r=1;
-        int sum=0;
-        for(int i=0;i<nums.length;i++){
-            if(nums[l]+nums[r]==target){
-                arr[0]=l;
-                arr[1]=r;
-                break;
-            }else{
-                l++;
-                r++;
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    return new int[] { i, j };
+                }
             }
         }
-        return arr;
+        return new int[0];
     }
 }
