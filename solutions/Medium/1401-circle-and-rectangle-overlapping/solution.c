184e7caec923983567b14ccd2fc50f659e0ec0b8
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-19 16:49:07 +0000

    leetie: Add 1401. Circle and Rectangle Overlapping [Medium] (c)

--- solutions/Medium/1401-circle-and-rectangle-overlapping/solution.c
+++ solutions/Medium/1401-circle-and-rectangle-overlapping/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Geometry
 // Link     : https://leetcode.com/problems/circle-and-rectangle-overlapping/
-// Runtime  : 0 ms (beats 100%)
-// Memory   : 8364000 (beats 94%)
+// Runtime  : 0 ms (beats 0%)
+// Memory   : 8628000 (beats 0%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
