24cf6ad50197ab133daaec16a9a91240258b4629
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-20 11:20:37 +0000

    leetie: Add 319. Bulb Switcher [Medium] (c)

--- solutions/Medium/0319-bulb-switcher/solution.c
+++ solutions/Medium/0319-bulb-switcher/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Math, Brainteaser
 // Link     : https://leetcode.com/problems/bulb-switcher/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8432000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 8436000 (beats 73%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
