19b5673f58aa327ae0a319019f06c13945832053
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-21 06:26:49 +0000

    leetie: Add 978. Longest Turbulent Subarray [Medium] (c)

--- solutions/Medium/0978-longest-turbulent-subarray/solution.c
+++ solutions/Medium/0978-longest-turbulent-subarray/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Dynamic Programming, Sliding Window
 // Link     : https://leetcode.com/problems/longest-turbulent-subarray/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8372000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 11836000 (beats 33%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
