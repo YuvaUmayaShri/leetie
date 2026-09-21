891523dc2e5aff8ff3086d3a31d02df870333a8b
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-21 06:25:47 +0000

    leetie: Add 567. Permutation in String [Medium] (c)

--- solutions/Medium/0567-permutation-in-string/solution.c
+++ solutions/Medium/0567-permutation-in-string/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Hash Table, Two Pointers, String, Sliding Window
 // Link     : https://leetcode.com/problems/permutation-in-string/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8312000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 9108000 (beats 31%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
