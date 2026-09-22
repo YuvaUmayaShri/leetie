9b0ca1226e63c394debb5c50a68d0d9dfc0c3c36
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-22 05:57:45 +0000

    leetie: Add 665. Non-decreasing Array [Medium] (c)

--- solutions/Medium/0665-non-decreasing-array/solution.c
+++ solutions/Medium/0665-non-decreasing-array/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array
 // Link     : https://leetcode.com/problems/non-decreasing-array/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8600000 (beats 0%)
+// Runtime  : 0 ms (beats 100%)
+// Memory   : 10092000 (beats 41%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
