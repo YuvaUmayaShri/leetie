48fdb56a7997b922a11644b5bb506c93f1ee7d82
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-22 05:53:17 +0000

    leetie: Add 587. Erect the Fence [Hard] (c)

--- solutions/Hard/0587-erect-the-fence/solution.c
+++ solutions/Hard/0587-erect-the-fence/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Hard
 // Tags     : Array, Math, Geometry, Convex Hull, Polygons
 // Link     : https://leetcode.com/problems/erect-the-fence/
-// Runtime  : 0 ms (beats 0%)
-// Memory   : 8696000 (beats 0%)
+// Runtime  : 10 ms (beats 0%)
+// Memory   : 16140000 (beats 60%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
