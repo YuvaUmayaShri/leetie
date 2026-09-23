1eaa638f7c145bba990666dd66343eb7edcd826f
YuvaUmayaShri <yuvaumayashri@users.noreply.github.com>
2026-09-23 17:13:00 +0000

    leetie: Add 1658. Minimum Operations to Reduce X to Zero [Medium] (c)

--- solutions/Medium/1658-minimum-operations-to-reduce-x-to-zero/solution.c
+++ solutions/Medium/1658-minimum-operations-to-reduce-x-to-zero/solution.c
@@ -3,8 +3,8 @@
 // Difficulty: Medium
 // Tags     : Array, Hash Table, Binary Search, Sliding Window, Prefix Sum
 // Link     : https://leetcode.com/problems/minimum-operations-to-reduce-x-to-zero/
-// Runtime  : 5 ms (beats 24%)
-// Memory   : 17308000 (beats 2%)
+// Runtime  : 7 ms (beats 16%)
+// Memory   : 17136000 (beats 71%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
