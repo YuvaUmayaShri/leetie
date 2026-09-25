6f3296706f28682b5607b0d5eb734fa8e2ed3e55
YuvaUmayaShri <YuvaUmayaShri@users.noreply.github.com>
2026-09-25 09:06:35 +0000

    leetie: Add 1802. Maximum Value at a Given Index in a Bounded Array [Medium] (c)

--- solutions/Medium/1802-maximum-value-at-a-given-index-in-a-bounded-array/solution.c
+++ solutions/Medium/1802-maximum-value-at-a-given-index-in-a-bounded-array/solution.c
@@ -4,7 +4,7 @@
 // Tags     : Math, Binary Search, Greedy
 // Link     : https://leetcode.com/problems/maximum-value-at-a-given-index-in-a-bounded-array/
 // Runtime  : 0 ms (beats 100%)
-// Memory   : 8704000 (beats 100%)
+// Memory   : 8652000 (beats 100%)
 // Language : c
 // Copyright: (c) 2026 YuvaUmayaShri. All rights reserved.
 // Synced by: leetie
